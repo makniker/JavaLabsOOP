71224569281102a3297b0f8fa8c56580e6d4ca5b
makniker <102602253+makniker@users.noreply.github.com>
2023-02-28 16:14:35 +0300

    added args

--- lab2/ClassWithAnnotation.java
+++ lab2/ClassWithAnnotation.java
@@ -2,46 +2,46 @@ package lab2;
 
 public class ClassWithAnnotation {
     @MyAnnotation()
-    public void publicMethod1() {
-        System.out.println("publicMethod1");
+    public void publicMethod1(int a, int b) {
+        System.out.println("publicMethod1: " + a + " " + b);
     }
 
     @MyAnnotation(numberOfCalls = 3)
-    public void publicMethod2() {
-        System.out.println("publicMethod2");
+    public void publicMethod2(int a, int b) {
+        System.out.println("publicMethod2: " + a + " " + b);
     }
 
     @MyAnnotation(numberOfCalls = 6)
-    public void publicMethod3() {
-        System.out.println("publicMethod3");
+    public void publicMethod3(int a, int b) {
+        System.out.println("publicMethod3: " + a + " " + b);
     }
 
     @MyAnnotation
-    private void privateMethod1() {
-        System.out.println("privateMethod1");
+    private void privateMethod1(int a, int b) {
+        System.out.println("privateMethod1: " + a + " " + b);
     }
     @MyAnnotation(numberOfCalls = 3)
-    private void privateMethod2() {
-        System.out.println("privateMethod2");
+    private void privateMethod2(int a, int b) {
+        System.out.println("privateMethod2: " + a + " " + b);
     }
 
     @MyAnnotation()
-    private void privateMethod3() {
-        System.out.println("privateMethod3");
+    private void privateMethod3(int a, int b) {
+        System.out.println("privateMethod3: " + a + " " + b);
     }
 
     @MyAnnotation(numberOfCalls = 3)
-    protected void protectedMethod1(){
-        System.out.println("protectedMethod1");
+    protected void protectedMethod1(int a, int b){
+        System.out.println("protectedMethod1: " + a + " " + b);
     }
 
     @MyAnnotation(numberOfCalls = 3)
-    protected void protectedMethod2(){
-        System.out.println("protectedMethod2");
+    protected void protectedMethod2(int a, int b){
+        System.out.println("protectedMethod2: " + a + " " + b);
     }
 
     @MyAnnotation(numberOfCalls = 2)
-    protected void protectedMethod3(){
-        System.out.println("protectedMethod3");
+    protected void protectedMethod3(int a, int b){
+        System.out.println("protectedMethod3: " + a + " " + b);
     }
 }

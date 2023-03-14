40a6fade3c12f733764366583aec24fe95eacede
makniker <102602253+makniker@users.noreply.github.com>
2023-03-14 10:55:04 +0300

    fixed lab2

--- lab2/package2/Annotations.java
+++ lab2/package2/Annotations.java
@@ -1,12 +1,17 @@
 package lab2.package2;
 
+import java.lang.reflect.InvocationTargetException;
+
 public class Annotations {
     public static void main(String[] args){
         Caller caller = new Caller();
         try {
             caller.call();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+        } catch (InvocationTargetException e1) {
+            System.out.println("Invoke exception");
+        }
+        catch (IllegalAccessException e2) {
+            System.out.println("IllegalAccess, cant get method");
         }
     }
 }

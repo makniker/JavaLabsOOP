64f60c49bb08884e330a11a09aa70a8f9d5a3ec2
makniker <102602253+makniker@users.noreply.github.com>
2023-04-23 23:43:46 +0300

    fixed names in lab3

--- lab3/Animals.java
+++ lab3/Animals.java
@@ -4,60 +4,60 @@ import java.util.ArrayList;
 
 public class Animals {
     public static void main(String[] args) {
-        ArrayList<Mammals> SrcCollection1 = new ArrayList<>(9);
-        SrcCollection1.add(new Manul("manul1"));
-        SrcCollection1.add(new Manul("manul2"));
-        SrcCollection1.add(new Manul("manul3"));
-        SrcCollection1.add(new Lynx("lynx1"));
-        SrcCollection1.add(new Lynx("lynx2"));
-        SrcCollection1.add(new Lynx("lynx3"));
-        SrcCollection1.add(new EuropeanHedgehog("hedgehog1"));
-        SrcCollection1.add(new EuropeanHedgehog("hedgehog2"));
-        SrcCollection1.add(new EuropeanHedgehog("hedgehog3"));
+        ArrayList<Mammals> srcCollection1 = new ArrayList<>(9);
+        srcCollection1.add(new Manul("manul1"));
+        srcCollection1.add(new Manul("manul2"));
+        srcCollection1.add(new Manul("manul3"));
+        srcCollection1.add(new Lynx("lynx1"));
+        srcCollection1.add(new Lynx("lynx2"));
+        srcCollection1.add(new Lynx("lynx3"));
+        srcCollection1.add(new EuropeanHedgehog("hedgehog1"));
+        srcCollection1.add(new EuropeanHedgehog("hedgehog2"));
+        srcCollection1.add(new EuropeanHedgehog("hedgehog3"));
 
-        ArrayList<Hedgehogs> CollectionOfHedgehogs = new ArrayList<>(3);
-        ArrayList<Feline> CollectionOfFeline = new ArrayList<>(3);
-        ArrayList<Predatory> CollectionOfPredators = new ArrayList<>(3);
+        ArrayList<Hedgehogs> collectionOfHedgehogs = new ArrayList<>(3);
+        ArrayList<Feline> collectionOfFeline = new ArrayList<>(3);
+        ArrayList<Predatory> collectionOfPredators = new ArrayList<>(3);
 
-        segregate(SrcCollection1, CollectionOfHedgehogs, CollectionOfFeline, CollectionOfPredators);
+        segregate(srcCollection1, collectionOfHedgehogs, collectionOfFeline, collectionOfPredators);
 
-        System.out.println(SrcCollection1);
-        System.out.println(CollectionOfFeline);
-        System.out.println(CollectionOfHedgehogs);
-        System.out.println(CollectionOfPredators);
+        System.out.println(srcCollection1);
+        System.out.println(collectionOfFeline);
+        System.out.println(collectionOfHedgehogs);
+        System.out.println(collectionOfPredators);
 
-        ArrayList<Predatory> SrcCollection2 = new ArrayList<>(9);
-        SrcCollection2.add(new Manul("manul1"));
-        SrcCollection2.add(new Manul("manul2"));
-        SrcCollection2.add(new Manul("manul3"));
-        SrcCollection2.add(new Lynx("lynx1"));
-        SrcCollection2.add(new Lynx("lynx2"));
-        SrcCollection2.add(new Lynx("lynx3"));
+        ArrayList<Predatory> srcCollection2 = new ArrayList<>(9);
+        srcCollection2.add(new Manul("manul1"));
+        srcCollection2.add(new Manul("manul2"));
+        srcCollection2.add(new Manul("manul3"));
+        srcCollection2.add(new Lynx("lynx1"));
+        srcCollection2.add(new Lynx("lynx2"));
+        srcCollection2.add(new Lynx("lynx3"));
 
-        ArrayList<Chordates> CollectionOfChordates = new ArrayList<>(3);
-        ArrayList<Manul> CollectionOfManuls = new ArrayList<>(3);
-        ArrayList<Feline> CollectionOfFeline2 = new ArrayList<>(3);
-        segregate(SrcCollection2, CollectionOfChordates, CollectionOfManuls, CollectionOfFeline2);
+        ArrayList<Chordates> collectionOfChordates = new ArrayList<>(3);
+        ArrayList<Manul> collectionOfManuls = new ArrayList<>(3);
+        ArrayList<Feline> collectionOfFeline2 = new ArrayList<>(3);
+        segregate(srcCollection2, collectionOfChordates, collectionOfManuls, collectionOfFeline2);
 
-        System.out.println(SrcCollection2);
-        System.out.println(CollectionOfChordates);
-        System.out.println(CollectionOfManuls);
-        System.out.println(CollectionOfFeline2);
+        System.out.println(srcCollection2);
+        System.out.println(collectionOfChordates);
+        System.out.println(collectionOfManuls);
+        System.out.println(collectionOfFeline2);
 
-        ArrayList<Hedgehogs> SrcCollection3 = new ArrayList<>(9);
-        SrcCollection3.add(new EuropeanHedgehog("hedgehog1"));
-        SrcCollection3.add(new EuropeanHedgehog("hedgehog2"));
-        SrcCollection3.add(new EuropeanHedgehog("hedgehog3"));
+        ArrayList<Hedgehogs> srcCollection3 = new ArrayList<>(9);
+        srcCollection3.add(new EuropeanHedgehog("hedgehog1"));
+        srcCollection3.add(new EuropeanHedgehog("hedgehog2"));
+        srcCollection3.add(new EuropeanHedgehog("hedgehog3"));
 
-        ArrayList<Insectivores> CollectionOfInsectivores = new ArrayList<>(3);
-        ArrayList<Feline> CollectionOfFeline3 = new ArrayList<>(3);
-        ArrayList<Feline> CollectionOfFeline4 = new ArrayList<>(3);
-        segregate(SrcCollection2, CollectionOfInsectivores, CollectionOfFeline3, CollectionOfFeline4);
+        ArrayList<Insectivores> collectionOfInsectivores = new ArrayList<>(3);
+        ArrayList<Feline> collectionOfFeline3 = new ArrayList<>(3);
+        ArrayList<Feline> collectionOfFeline4 = new ArrayList<>(3);
+        segregate(srcCollection2, collectionOfInsectivores, collectionOfFeline3, collectionOfFeline4);
 
-        System.out.println(SrcCollection3);
-        System.out.println(CollectionOfChordates);
-        System.out.println(CollectionOfManuls);
-        System.out.println(CollectionOfFeline2);
+        System.out.println(srcCollection3);
+        System.out.println(collectionOfChordates);
+        System.out.println(collectionOfManuls);
+        System.out.println(collectionOfFeline2);
     }
 
     public static void segregate(ArrayList<? extends Chordates> SrcCollection,
